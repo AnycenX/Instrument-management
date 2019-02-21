adc1dfa9036ccbfdc28cba4f967d5e23188f6678
Aravent <x@aravent.cn>
2019-02-21 19:48:13 +0800

    socket

---
+++ Instrument-management/frmSetting.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Instrument_management
+{
+    partial class frmSetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "扫描局域网";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(12, 48);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(118, 160);
+            this.listBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "TCP连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 21);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 21);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "8888";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 102);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 70);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "user:sss";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(136, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 30);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "发送数据";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmSetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 360);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmSetting";
+            this.Text = "系统设置";
+            this.Load += new System.EventHandler(this.frmSetting_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Instrument-management/frmSetting.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace Instrument_management
+{
+    public partial class frmSetting : Form
+    {
+        string[] localip = new string[20];
+        Socket clientSocket;
+       // new Thread(receiveMsg).Start(this);
+
+        public frmSetting()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void frmSetting_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnumComputers(string ip3)
+        {
+            try
+            {
+                for (int i = 1; i <= 255; i++)
+                {
+                    Ping myPing;
+                    myPing = new Ping();
+                    myPing.PingCompleted += new PingCompletedEventHandler(_myPing_PingCompleted);
+
+                    string pingIP = "192.168." + ip3 + "." + i.ToString();
+                    myPing.SendAsync(pingIP, 1000, null);
+                }
+            }
+            catch
+            {
+                listBox2.Items.Add("Error");
+            }
+        }
+
+        private async void _myPing_PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            if (e.Reply.Status == IPStatus.Success)
+            {
+                await Task.Run(() =>
+                {
+                    //Console.WriteLine(e.Reply.Address.ToString() + "|" + Dns.GetHostByAddress(IPAddress.Parse(e.Reply.Address.ToString())).HostName);
+                    listBox2.Items.Add(e.Reply.Address.ToString());
+                });
+            }
+        }
+
+        private void GetIP()
+        {
+            string hostName = Dns.GetHostName();//本机名   
+            IPAddress[] addressList = Dns.GetHostAddresses(hostName);//会返回所有地址，包括IPv4和IPv6   
+            foreach (IPAddress ip in addressList)  
+            {
+                string[] ids = ip.ToString().Split('.');
+                if (ids[0] == "192" && ids[1] == "168" && ids[3] != "1")
+                {
+                    EnumComputers(ids[2]);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetIP();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "TCP连接")
+            {
+                createConn();
+            }
+            else
+            {
+                closeConn();
+            }
+        }
+
+        public bool createConn()
+        {
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Parse(textBox1.Text);       
+                int connPort = Int16.Parse(textBox2.Text);
+                clientSocket.Connect(new IPEndPoint(ip, connPort)); //配置服务器IP与端口  
+                listBox2.Items.Add("连接服务器成功");
+                button1.Text = "断开连接";
+            }
+            catch (Exception e)
+            {
+                //listBox2.Items.Add("连接服务器失败" + e.ToString());
+                MessageBox.Show("连接服务器失败" + e.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        public bool closeConn()
+        {
+            try
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                Thread.Sleep(10);
+                clientSocket.Close();
+                listBox2.Items.Add("断开服务器成功");
+                button1.Text = "TCP连接";
+                //开启监听线程
+                Thread receiveThread = new Thread(receiveMsg);
+                receiveThread.Start(this);
+            }
+            catch (Exception e)
+            {
+                //listBox2.Items.Add("断开服务器失败" + e.ToString());
+                MessageBox.Show("断开服务器失败" + e.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        private bool sendSignal(string signal)
+        {
+            if (clientSocket == null)
+            {
+                listBox2.Items.Add("发送失败，未连接客户端");
+                return false;
+            }
+            try
+            {
+                clientSocket.Send(Encoding.UTF8.GetBytes(signal));
+                return true;
+            }
+            catch (Exception e)
+            {
+                listBox2.Items.Add("发送失败" + e.ToString());
+                clientSocket = null;
+            }
+            return false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sendSignal(textBox3.Text);
+        }
+
+    }
+}
