be19e60d836fc42a7e483d4e4b45bff5f8751738
Aravent <x@aravent.cn>
2019-02-21 17:13:59 +0800

    UI

--- Instrument-management/Form1.Designer.cs
+++ Instrument-management/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Instrument_management
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,20 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.TimKill = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnLoading = new System.Windows.Forms.Button();
+            this.btnShutdown = new System.Windows.Forms.Button();
+            this.PlanLoad = new System.Windows.Forms.Panel();
+            this.picBack = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlanLoad.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // TimKill
+            // 
+            this.TimKill.Enabled = true;
+            this.TimKill.Tick += new System.EventHandler(this.TimKill_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "禁用任务管理器";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "启用任务管理器";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Silver;
+            this.textBox1.Location = new System.Drawing.Point(22, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 29);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "请输入用户名";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.Silver;
+            this.textBox2.Location = new System.Drawing.Point(22, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(255, 29);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "请输入密码";
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // btnLoading
+            // 
+            this.btnLoading.Location = new System.Drawing.Point(83, 140);
+            this.btnLoading.Name = "btnLoading";
+            this.btnLoading.Size = new System.Drawing.Size(131, 38);
+            this.btnLoading.TabIndex = 6;
+            this.btnLoading.Text = "登录";
+            this.btnLoading.UseVisualStyleBackColor = true;
+            // 
+            // btnShutdown
+            // 
+            this.btnShutdown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnShutdown.Location = new System.Drawing.Point(12, 454);
+            this.btnShutdown.Name = "btnShutdown";
+            this.btnShutdown.Size = new System.Drawing.Size(51, 34);
+            this.btnShutdown.TabIndex = 7;
+            this.btnShutdown.Text = "关闭";
+            this.btnShutdown.UseVisualStyleBackColor = true;
+            this.btnShutdown.Click += new System.EventHandler(this.btnShutdown_Click);
+            // 
+            // PlanLoad
+            // 
+            this.PlanLoad.BackColor = System.Drawing.SystemColors.Control;
+            this.PlanLoad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PlanLoad.Controls.Add(this.label1);
+            this.PlanLoad.Controls.Add(this.btnLoading);
+            this.PlanLoad.Controls.Add(this.textBox1);
+            this.PlanLoad.Controls.Add(this.textBox2);
+            this.PlanLoad.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PlanLoad.Location = new System.Drawing.Point(450, 155);
+            this.PlanLoad.Name = "PlanLoad";
+            this.PlanLoad.Size = new System.Drawing.Size(300, 196);
+            this.PlanLoad.TabIndex = 8;
+            // 
+            // picBack
+            // 
+            this.picBack.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBack.Image = ((System.Drawing.Image)(resources.GetObject("picBack.Image")));
+            this.picBack.Location = new System.Drawing.Point(0, 0);
+            this.picBack.Name = "picBack";
+            this.picBack.Size = new System.Drawing.Size(1000, 500);
+            this.picBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBack.TabIndex = 9;
+            this.picBack.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "请先登录";
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1054, 526);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.PlanLoad);
+            this.Controls.Add(this.btnShutdown);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.Text = "屏幕锁定";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.PlanLoad.ResumeLayout(false);
+            this.PlanLoad.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBack)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        public System.Windows.Forms.Timer TimKill;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnLoading;
+        private System.Windows.Forms.Button btnShutdown;
+        private System.Windows.Forms.Panel PlanLoad;
+        private System.Windows.Forms.PictureBox picBack;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Instrument-management/Form1.cs
+++ Instrument-management/Form1.cs
@@ -3,17 +3,167 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+using System.Drawing.Drawing2D;
+using System.Management;
+using System.Management.Instrumentation;
+using System.Runtime.InteropServices;
 
 namespace Instrument_management
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        Hook h = new Hook();
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+        public frmMain()
         {
             InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            FullScreen();//最大化
+            h.Hook_Start();//禁用快捷键
+            SetWindowPos(this.Handle, -1, 0, 0, 0, 0, 1 | 2);//保持窗体最前
+            Locations();
+            //Translate();
+        }
+
+        protected override bool ProcessKeyEventArgs(ref Message m)//禁用任务管理器
+        {
+            KillTaskmgr();
+            return base.ProcessKeyEventArgs(ref m);
+        }
+
+        protected override void WndProc(ref Message m)//禁用鼠标右键
+        {
+            if (m.Msg == 0x205)
+            {
+
+            }
+            base.WndProc(ref m);
+        }
+
+        private void KillTaskmgr()//禁用任务管理器
+        {
+            Process[] sum = Process.GetProcesses();
+            foreach (Process p in sum)
+            {
+                if (p.ProcessName == "taskmgr" || p.ProcessName == "cmd")
+                    try
+                    {
+                        p.Kill();
+                    }
+                    catch
+                    {
+                        ;
+                    }
+            }
+        }
+
+        private void FullScreen()//全屏
+        {
+            this.SetVisibleCore(true);
+        }
+
+        private void TimKill_Tick(object sender, EventArgs e)//始终禁用，对xp有效
+        {
+            KillTaskmgr();
+        }
+
+        public void Locations()//控件相对于屏幕位置
+        {
+            Rectangle ScreenArea = System.Windows.Forms.Screen.GetBounds(this);
+            int width = ScreenArea.Width; //屏幕宽度 
+            int height = ScreenArea.Height;
+            PlanLoad.Location = new Point((width - 300) / 2, (height - 300) / 2);
+        }
+
+        public void Translate()
+        {
+            PlanLoad.Parent = picBack;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey rk = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+                rk.SetValue("DisableTaskMgr", 1, Microsoft.Win32.RegistryValueKind.DWord);
+                MessageBox.Show("禁用任务管理器成功");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey rk = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+                rk.SetValue("DisableTaskMgr", 0, Microsoft.Win32.RegistryValueKind.DWord);
+                MessageBox.Show("启用任务管理器成功");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShutdown_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "请输入密码";
+                textBox2.ForeColor = Color.Silver;
+                textBox2.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "请输入密码")
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor = Color.Black;
+                textBox2.UseSystemPasswordChar = true;
+            }   
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "请输入用户名")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "请输入用户名";
+                textBox1.ForeColor = Color.Silver;
+            }
         }
     }
 }
